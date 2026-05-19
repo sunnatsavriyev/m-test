ff3a9e63c748f030c8543cf84f7ca433afa0e30d
Sunnat Savriyev <sunnatchiksavriyev@gmail.com>
2026-05-19 08:42:10 +0500

    fix edited options

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -12,7 +12,22 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'first_name', 'last_name', 'role', 'department', 'department_name', 'avatar', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'required': False,
+                'allow_blank': True,
+                'allow_null': True
+            }
+        }
+
+    def validate(self, attrs):
+        # Yaratish (create) vaqtida parol kiritilishi majburiy
+        if not self.instance:
+            password = attrs.get('password')
+            if not password:
+                raise serializers.ValidationError({"password": "Parol bo'sh bo'lishi mumkin emas."})
+        return attrs
 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
@@ -21,6 +36,15 @@ class UserSerializer(serializers.ModelSerializer):
             user.set_password(password)
         user.save()
         return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
 
 class ProfileUpdateSerializer(serializers.ModelSerializer):
     class Meta:
